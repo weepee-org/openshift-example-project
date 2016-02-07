15229ec4b41f2f55cc0682d02194e534b3554943
Joeri van Dooren <joeri@weepee.org>
2016-02-07 19:33:17 +0100

    fix

--- mojo/app/mojo.pl
+++ mojo/app/mojo.pl
@@ -8,13 +8,6 @@ use utf8;
 use Mojolicious::Lite;
 use Mojo::JSON qw(decode_json);
 use Sys::Hostname;
-use Redis::Fast;
-
-my $redis = Redis::Fast->new(
-      reconnect => 60,
-      every => 500_000,
-      server => 'redis2:6379'
-);
 
 get '/' => sub {
   my $self = shift;
