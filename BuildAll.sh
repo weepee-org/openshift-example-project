de7e2ac39d460b66ac15ff583a804f338d1bf8de
Joeri van Dooren <ure@moreorless.be>
2016-02-09 11:41:05 +0100

    added wordpress-hhvm

--- BuildAll.sh
+++ BuildAll.sh
@@ -53,3 +53,18 @@ PASSWORD=$(openssl rand 12 -base64)
 echo "DB wordpress USER wordpress PASSWORD ${PASSWORD}"
 oc process -f wordpress-php/MysqlTemplate.yaml -v MYSQL_DATABASE=wordpress,VOLUME_CAPACITY=512Mi,MYSQL_USER=wordpress,MYSQL_PASSWORD=${PASSWORD} | oc create -f -
 oc start-build wordpress
+
+echo "wordpress hhvm (replace php)"
+oc delete -f wordpress-php/BuildConfig.yaml
+oc create -f Gluster-Service.yaml
+oc create -f Gluster-Endpoints.yaml
+oc create -f wordpress-hhvm/PersistentVolumeClaim.yaml
+oc create -f wordpress-hhvm/BuildConfig.yaml
+oc create -f wordpress-hhvm/ImageStream.yaml
+oc create -f wordpress-hhvm/DeploymentConfig.yaml
+oc create -f wordpress-hhvm/Services.yaml
+oc create -f wordpress-hhvm/Route.yaml
+# PASSWORD=$(openssl rand 12 -base64)
+# echo "DB wordpress USER wordpress PASSWORD ${PASSWORD}"
+# oc process -f wordpress-php/MysqlTemplate.yaml -v MYSQL_DATABASE=wordpress,VOLUME_CAPACITY=512Mi,MYSQL_USER=wordpress,MYSQL_PASSWORD=${PASSWORD} | oc create -f -
+# oc start-build wordpress
