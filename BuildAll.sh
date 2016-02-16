faba728ad9a771a8e012a103a843e197c2c8a76f
Joeri van Dooren <ure@moreorless.be>
2016-02-16 16:28:30 +0100

    test phppgadmin

--- BuildAll.sh
+++ BuildAll.sh
@@ -71,14 +71,14 @@ oc create -f wordpress-hhvm/DeploymentConfig.yaml
 oc create -f wordpress-hhvm/Services.yaml
 
 echo "wordpress hhvm"
-# oc create -f Gluster-Service.yaml
-# oc create -f Gluster-Endpoints.yaml
-# oc create -f wordpress-hhvm/PersistentVolumeClaim.yaml
-# oc create -f wordpress-hhvm/BuildConfig.yaml
-# oc create -f wordpress-hhvm/ImageStream.yaml
-# oc create -f wordpress-hhvm/DeploymentConfig.yaml
-# oc create -f wordpress-hhvm/Services.yaml
-# oc create -f wordpress-hhvm/Route.yaml
+oc create -f Gluster-Service.yaml
+oc create -f Gluster-Endpoints.yaml
+oc create -f wordpress-hhvm/PersistentVolumeClaim.yaml
+oc create -f wordpress-hhvm/BuildConfig.yaml
+oc create -f wordpress-hhvm/ImageStream.yaml
+oc create -f wordpress-hhvm/DeploymentConfig.yaml
+oc create -f wordpress-hhvm/Services.yaml
+oc create -f wordpress-hhvm/Route.yaml
 # PASSWORD=$(openssl rand 12 -base64)
 # echo "DB wordpress USER wordpress PASSWORD ${PASSWORD}"
 # oc process -f wordpress-php/MysqlTemplate.yaml -v MYSQL_DATABASE=wordpress,VOLUME_CAPACITY=512Mi,MYSQL_USER=wordpress,MYSQL_PASSWORD=${PASSWORD} | oc create -f -
